c5018102785249269913985aead53d53e3ba8b37
Muhammad Affandes <affandes@uin-suska.ac.id>
2026-03-13 13:52:05 +0700

    init

---
+++ book.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+
+class Book:
+    def __init__(self, id, title, author):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.last_updated = datetime.now()
---
+++ main.py
@@ -0,0 +1,7 @@
+from book import Book
+
+b1 = Book(1, "Learn Python", "Affan")
+b2 = Book(2, "Mastering OOP", "Budi")
+
+print(b1.title)
+print(b2.title)
